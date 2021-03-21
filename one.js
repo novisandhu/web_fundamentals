b10034be606e11c1a24a49c447111b465516db1a
unknown <you@example.com>
2021-03-21 15:32:14 +0530

    First Commit

---
+++ one.js
@@ -0,0 +1,19 @@
+// function go1() {
+//   //   document.getElementById("s1").innerText = "hello world";
+//   document.getElementById("s1").innerHTML = "<mark>hello</mark>";
+// }
+
+// function go2() {
+//   var a = 10;
+//   var b = 60;
+//   var c = a + b;
+//   alert(c);
+// }
+function go3() {
+  var a = parseInt(document.getElementById("a").Value);
+  var b = parseInt(document.getElementById("b").Value);
+  alert(typeof a);
+  alert(typeof b);
+  var d = a + b;
+  alert(d);
+}
